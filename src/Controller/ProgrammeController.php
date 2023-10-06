fa97da7fba4085cf111ec2c34a8ecf508a6cb936
Djioliat <djioliat@gmail.com>
2023-10-06 23:33:53 +0200

    edit et delete animation

--- src/Controller/ProgrammeController.php
+++ src/Controller/ProgrammeController.php
@@ -31,4 +31,33 @@ class ProgrammeController extends AbstractController
             'form' => $form
         ]);
     }
+
+    #[Route('/programme/{id}/edit', name: 'app_programme_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager, $id): Response
+    {
+        $programme = $entityManager->getRepository(Programme::class)->find($id);
+
+        $form = $this->createForm(ProgrammeType::class, $programme);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('programme/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/animation/{id}/delete', name: 'app_programme_delete')]
+    public function delete(EntityManagerInterface $entityManager, Programme $programme): Response
+    {
+        $entityManager->remove($programme);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
 }
